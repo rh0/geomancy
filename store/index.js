9087f0206b5a12ebc92634fa0d37848209eb1f9a
rho <ryan@theoleschool.com>
2020-07-05 20:02:38 -0500

    arranging figures in binary order

--- store/index.js
+++ store/index.js
@@ -1,5 +1,23 @@
 export const state = () => ({
+  // All Geomanic figures in binary order so its index will match its binary
+  // representation. For instance:
+  //
+  // cards[3] lines are binary 3 (0011)
   cards: [
+    {
+      name: 'Via',
+      numeral: 'XVI',
+      lines: [0,0,0,0],
+      sign: '♋︎',
+      planet: '☽'
+    },
+    {
+      name: 'Cauda Draconis',
+      numeral: 'XIII',
+      lines: [0,0,0,1],
+      sign: '☋',
+      planet: '♄♂︎'
+    },
     {
       name: 'Puer',
       numeral: 'I',
@@ -12,39 +30,11 @@ export const state = () => ({
       }
     },
     {
-      name: 'Amissio',
-      numeral: 'II',
-      lines: [0,1,0,1],
-      sign: '♉︎',
-      planet: '♀︎'
-    },
-    {
-      name: 'Albus',
-      numeral: 'III',
-      lines: [1,1,0,1],
-      sign: '♊︎',
-      planet: '☿'
-    },
-    {
-      name: 'Populus',
-      numeral: 'IV',
-      lines: [1,1,1,1],
-      sign: '♋︎',
-      planet: '☽'
-    },
-    {
-      name: 'Fortuna Major',
-      numeral: 'V',
-      lines: [1,1,0,0],
+      name: 'Fortuna Minor',
+      numeral: 'XV',
+      lines: [0,0,1,1],
       sign: '♌︎',
       planet: '☉'
-    },
-    {
-      name: 'Conjunctio',
-      numeral: 'VI',
-      lines: [1,0,0,1],
-      sign: '♍︎',
-      planet: '☿'
     },
     {
       name: 'Puella',
@@ -54,31 +44,17 @@ export const state = () => ({
       planet: '♀︎'
     },
     {
-      name: 'Rubeus',
-      numeral: 'VIII',
-      lines: [1,0,1,1],
-      sign: '♏︎',
-      planet: '♂︎'
-    },
-    {
-      name: 'Acquisitio',
-      numeral: 'IX',
-      lines: [1,0,1,0],
-      sign: '♐︎',
-      planet: '♃'
+      name: 'Amissio',
+      numeral: 'II',
+      lines: [0,1,0,1],
+      sign: '♉︎',
+      planet: '♀︎'
     },
     {
       name: 'Carcer',
       numeral: 'X',
       lines: [0,1,1,0],
       sign: '♑︎',
-      planet: '♄'
-    },
-    {
-      name: 'Tristitia',
-      numeral: 'XI',
-      lines: [1,1,1,0],
-      sign: '♒︎',
       planet: '♄'
     },
     {
@@ -89,13 +65,6 @@ export const state = () => ({
       planet: '♃'
     },
     {
-      name: 'Cauda Draconis',
-      numeral: 'XIII',
-      lines: [0,0,0,1],
-      sign: '☋',
-      planet: '♄♂︎'
-    },
-    {
       name: 'Caput Draconis',
       numeral: 'XIV',
       lines: [1,0,0,0],
@@ -103,16 +72,51 @@ export const state = () => ({
       planet: '♃♀︎'
     },
     {
-      name: 'Fortuna Minor',
-      numeral: 'XV',
-      lines: [0,0,1,1],
+      name: 'Conjunctio',
+      numeral: 'VI',
+      lines: [1,0,0,1],
+      sign: '♍︎',
+      planet: '☿'
+    },
+    {
+      name: 'Acquisitio',
+      numeral: 'IX',
+      lines: [1,0,1,0],
+      sign: '♐︎',
+      planet: '♃'
+    },
+    {
+      name: 'Rubeus',
+      numeral: 'VIII',
+      lines: [1,0,1,1],
+      sign: '♏︎',
+      planet: '♂︎'
+    },
+    {
+      name: 'Fortuna Major',
+      numeral: 'V',
+      lines: [1,1,0,0],
       sign: '♌︎',
       planet: '☉'
     },
     {
-      name: 'Via',
-      numeral: 'XVI',
-      lines: [0,0,0,0],
+      name: 'Albus',
+      numeral: 'III',
+      lines: [1,1,0,1],
+      sign: '♊︎',
+      planet: '☿'
+    },
+    {
+      name: 'Tristitia',
+      numeral: 'XI',
+      lines: [1,1,1,0],
+      sign: '♒︎',
+      planet: '♄'
+    },
+    {
+      name: 'Populus',
+      numeral: 'IV',
+      lines: [1,1,1,1],
       sign: '♋︎',
       planet: '☽'
     },

1e237056591b522a0ca3533e869702965464fae7
rho <ryan@theoleschool.com>
2020-07-05 21:40:45 -0500

    adding elements and styles

--- components/Card.vue
+++ components/Card.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="card">
-    <h1>{{ chosenFigure.numeral }}</h1>
-    <span class="sign">{{ chosenFigure.sign }}</span>
-    <span class="planet">{{ chosenFigure.planet }}</span>
+    <div class="card-header">
+      <h1>{{ chosenFigure.numeral }}</h1>
+      <span
+        class="element inner"
+        :class="[chosenFigure.elements.innerName]"
+      >
+        {{ chosenFigure.elements.inner }}
+      </span>
+      <span
+        class="element outer"
+        :class="[chosenFigure.elements.outerName]"
+      >
+        {{ chosenFigure.elements.outer }}
+      </span>
+    </div>
+
     <graph :lines="chosenFigure.lines" />
-    <h2>{{ chosenFigure.name }}</h2>
+
+    <div class="card-footer">
+      <h2 class="name">{{ chosenFigure.name }}</h2>
+      <span class="sign">{{ chosenFigure.sign }}</span>
+      <span class="planet">{{ chosenFigure.planet }}</span>
+    </div>
   </div>
 </template>
 
@@ -47,9 +65,60 @@ h1, h2 {
 
 h1 {
   letter-spacing: -3px;
+  margin: 0;
 }
 
 h2 {
   letter-spacing: 2.5px;
+  margin: 0;
 }
+
+.card-header {
+  margin: 10px 0 20px;
+}
+
+.card-footer {
+  margin: 20px 0 10px;
+}
+
+.card-header,
+.card-footer {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.card-header .inner,
+.card-footer .sign {
+  order: -1;
+}
+
+.element {
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+}
+
+.element.inner {
+  border: 1px solid #333;
+  border-radius: 10px;
+}
+
+.element.air,
+.element.earth {
+  text-decoration: line-through;
+}
+
+.element.fire,
+.element.air {
+  line-height: 14px;
+}
+
+.element.water,
+.element.earth {
+  line-height: 17px;
+}
+
 </style>
--- store/index.js
+++ store/index.js
@@ -1,22 +1,33 @@
 export const state = () => ({
   // All Geomanic figures in binary order so its index will match its binary
-  // representation. For instance:
-  //
-  // cards[3] lines are binary 3 (0011)
+  //   representation. For instance:
+  //   cards[3] lines are binary 3 (0011)
   cards: [
     {
       name: 'Via',
       numeral: 'XVI',
       lines: [0,0,0,0],
       sign: '♋︎',
-      planet: '☽'
+      planet: '☽',
+      elements: {
+        inner: '▽',
+        innerName: 'water',
+        outer: '▽',
+        outerName: 'water'
+      }
     },
     {
       name: 'Cauda Draconis',
       numeral: 'XIII',
       lines: [0,0,0,1],
       sign: '☋',
-      planet: '♄♂︎'
+      planet: '♄♂︎',
+      elements: {
+        inner: '△',
+        innerName: 'fire',
+        outer: '△',
+        outerName: 'fire'
+      }
     },
     {
       name: 'Puer',
@@ -25,8 +36,10 @@ export const state = () => ({
       sign: '♈︎',
       planet: '♂︎',
       elements: {
-        inner: '',
-        outer: ''
+        inner: '△',
+        innerName: 'air',
+        outer: '△',
+        outerName: 'fire'
       }
     },
     {
@@ -34,91 +47,169 @@ export const state = () => ({
       numeral: 'XV',
       lines: [0,0,1,1],
       sign: '♌︎',
-      planet: '☉'
+      planet: '☉',
+      elements: {
+        inner: '△',
+        innerName: 'fire',
+        outer: '△',
+        outerName: 'fire'
+      }
     },
     {
       name: 'Puella',
       numeral: 'VII',
       lines: [0,1,0,0],
       sign: '♎︎',
-      planet: '♀︎'
+      planet: '♀︎',
+      elements: {
+        inner: '▽',
+        innerName: 'water',
+        outer: '△',
+        outerName: 'air'
+      }
     },
     {
       name: 'Amissio',
       numeral: 'II',
       lines: [0,1,0,1],
       sign: '♉︎',
-      planet: '♀︎'
+      planet: '♀︎',
+      elements: {
+        inner: '△',
+        innerName: 'fire',
+        outer: '▽',
+        outerName: 'earth'
+      }
     },
     {
       name: 'Carcer',
       numeral: 'X',
       lines: [0,1,1,0],
       sign: '♑︎',
-      planet: '♄'
+      planet: '♄',
+      elements: {
+        inner: '▽',
+        innerName: 'earth',
+        outer: '▽',
+        outerName: 'earth'
+      }
     },
     {
       name: 'Lætitia',
       numeral: 'XII',
       lines: [0,1,1,1],
       sign: '♓︎',
-      planet: '♃'
+      planet: '♃',
+      elements: {
+        inner: '△',
+        innerName: 'fire',
+        outer: '▽',
+        outerName: 'water'
+      }
     },
     {
       name: 'Caput Draconis',
       numeral: 'XIV',
       lines: [1,0,0,0],
       sign: '☊',
-      planet: '♃♀︎'
+      planet: '♃♀︎',
+      elements: {
+        inner: '▽',
+        innerName: 'earth',
+        outer: '△',
+        outerName: 'fire'
+      }
     },
     {
       name: 'Conjunctio',
       numeral: 'VI',
       lines: [1,0,0,1],
       sign: '♍︎',
-      planet: '☿'
+      planet: '☿',
+      elements: {
+        inner: '△',
+        innerName: 'air',
+        outer: '▽',
+        outerName: 'earth'
+      }
     },
     {
       name: 'Acquisitio',
       numeral: 'IX',
       lines: [1,0,1,0],
       sign: '♐︎',
-      planet: '♃'
+      planet: '♃',
+      elements: {
+        inner: '△',
+        innerName: 'air',
+        outer: '△',
+        outerName: 'fire'
+      }
     },
     {
       name: 'Rubeus',
       numeral: 'VIII',
       lines: [1,0,1,1],
       sign: '♏︎',
-      planet: '♂︎'
+      planet: '♂︎',
+      elements: {
+        inner: '△',
+        innerName: 'air',
+        outer: '▽',
+        outerName: 'water'
+      }
     },
     {
       name: 'Fortuna Major',
       numeral: 'V',
       lines: [1,1,0,0],
       sign: '♌︎',
-      planet: '☉'
+      planet: '☉',
+      elements: {
+        inner: '▽',
+        innerName: 'earth',
+        outer: '△',
+        outerName: 'fire'
+      }
     },
     {
       name: 'Albus',
       numeral: 'III',
       lines: [1,1,0,1],
       sign: '♊︎',
-      planet: '☿'
+      planet: '☿',
+      elements: {
+        inner: '▽',
+        innerName: 'water',
+        outer: '△',
+        outerName: 'air'
+      }
     },
     {
       name: 'Tristitia',
       numeral: 'XI',
       lines: [1,1,1,0],
       sign: '♒︎',
-      planet: '♄'
+      planet: '♄',
+      elements: {
+        inner: '▽',
+        innerName: 'earth',
+        outer: '△',
+        outerName: 'air'
+      }
     },
     {
       name: 'Populus',
       numeral: 'IV',
       lines: [1,1,1,1],
       sign: '♋︎',
-      planet: '☽'
+      planet: '☽',
+      elements: {
+        inner: '▽',
+        innerName: 'water',
+        outer: '▽',
+        outerName: 'water'
+      }
     },
   ]
 })

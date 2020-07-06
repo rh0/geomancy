a910fdb4197c00af94743c5c542b4b7907bcf330
rho <ryan@theoleschool.com>
2020-07-05 19:52:28 -0500

    moving array of figures to a global store

--- components/Card.vue
+++ components/Card.vue
@@ -18,124 +18,6 @@ export default {
   data: function () {
     return {
       chosenFigure: {},
-      cards: [
-        {
-          name: 'Puer',
-          numeral: 'I',
-          lines: [0,0,1,0],
-          sign: '♈︎',
-          planet: '♂︎',
-          elements: {
-            inner: '',
-            outer: ''
-          }
-        },
-        {
-          name: 'Amissio',
-          numeral: 'II',
-          lines: [0,1,0,1],
-          sign: '♉︎',
-          planet: '♀︎'
-        },
-        {
-          name: 'Albus',
-          numeral: 'III',
-          lines: [1,1,0,1],
-          sign: '♊︎',
-          planet: '☿'
-        },
-        {
-          name: 'Populus',
-          numeral: 'IV',
-          lines: [1,1,1,1],
-          sign: '♋︎',
-          planet: '☽'
-        },
-        {
-          name: 'Fortuna Major',
-          numeral: 'V',
-          lines: [1,1,0,0],
-          sign: '♌︎',
-          planet: '☉'
-        },
-        {
-          name: 'Conjunctio',
-          numeral: 'VI',
-          lines: [1,0,0,1],
-          sign: '♍︎',
-          planet: '☿'
-        },
-        {
-          name: 'Puella',
-          numeral: 'VII',
-          lines: [0,1,0,0],
-          sign: '♎︎',
-          planet: '♀︎'
-        },
-        {
-          name: 'Rubeus',
-          numeral: 'VIII',
-          lines: [1,0,1,1],
-          sign: '♏︎',
-          planet: '♂︎'
-        },
-        {
-          name: 'Acquisitio',
-          numeral: 'IX',
-          lines: [1,0,1,0],
-          sign: '♐︎',
-          planet: '♃'
-        },
-        {
-          name: 'Carcer',
-          numeral: 'X',
-          lines: [0,1,1,0],
-          sign: '♑︎',
-          planet: '♄'
-        },
-        {
-          name: 'Tristitia',
-          numeral: 'XI',
-          lines: [1,1,1,0],
-          sign: '♒︎',
-          planet: '♄'
-        },
-        {
-          name: 'Lætitia',
-          numeral: 'XII',
-          lines: [0,1,1,1],
-          sign: '♓︎',
-          planet: '♃'
-        },
-        {
-          name: 'Cauda Draconis',
-          numeral: 'XIII',
-          lines: [0,0,0,1],
-          sign: '☋',
-          planet: '♄♂︎'
-        },
-        {
-          name: 'Caput Draconis',
-          numeral: 'XIV',
-          lines: [1,0,0,0],
-          sign: '☊',
-          planet: '♃♀︎'
-        },
-        {
-          name: 'Fortuna Minor',
-          numeral: 'XV',
-          lines: [0,0,1,1],
-          sign: '♌︎',
-          planet: '☉'
-        },
-        {
-          name: 'Via',
-          numeral: 'XVI',
-          lines: [0,0,0,0],
-          sign: '♋︎',
-          planet: '☽'
-        },
-      ],
       timer: ''
     }
   },
@@ -145,7 +27,7 @@ export default {
   },
   methods: {
     randomize () {
-      this.chosenFigure = this.cards[Math.floor(Math.random() * Math.floor(16))]
+      this.chosenFigure = this.$store.state.cards[Math.floor(Math.random() * Math.floor(16))]
     }
   },
   beforeDestroy: function () {
---
+++ store/index.js
@@ -0,0 +1,120 @@
+export const state = () => ({
+  cards: [
+    {
+      name: 'Puer',
+      numeral: 'I',
+      lines: [0,0,1,0],
+      sign: '♈︎',
+      planet: '♂︎',
+      elements: {
+        inner: '',
+        outer: ''
+      }
+    },
+    {
+      name: 'Amissio',
+      numeral: 'II',
+      lines: [0,1,0,1],
+      sign: '♉︎',
+      planet: '♀︎'
+    },
+    {
+      name: 'Albus',
+      numeral: 'III',
+      lines: [1,1,0,1],
+      sign: '♊︎',
+      planet: '☿'
+    },
+    {
+      name: 'Populus',
+      numeral: 'IV',
+      lines: [1,1,1,1],
+      sign: '♋︎',
+      planet: '☽'
+    },
+    {
+      name: 'Fortuna Major',
+      numeral: 'V',
+      lines: [1,1,0,0],
+      sign: '♌︎',
+      planet: '☉'
+    },
+    {
+      name: 'Conjunctio',
+      numeral: 'VI',
+      lines: [1,0,0,1],
+      sign: '♍︎',
+      planet: '☿'
+    },
+    {
+      name: 'Puella',
+      numeral: 'VII',
+      lines: [0,1,0,0],
+      sign: '♎︎',
+      planet: '♀︎'
+    },
+    {
+      name: 'Rubeus',
+      numeral: 'VIII',
+      lines: [1,0,1,1],
+      sign: '♏︎',
+      planet: '♂︎'
+    },
+    {
+      name: 'Acquisitio',
+      numeral: 'IX',
+      lines: [1,0,1,0],
+      sign: '♐︎',
+      planet: '♃'
+    },
+    {
+      name: 'Carcer',
+      numeral: 'X',
+      lines: [0,1,1,0],
+      sign: '♑︎',
+      planet: '♄'
+    },
+    {
+      name: 'Tristitia',
+      numeral: 'XI',
+      lines: [1,1,1,0],
+      sign: '♒︎',
+      planet: '♄'
+    },
+    {
+      name: 'Lætitia',
+      numeral: 'XII',
+      lines: [0,1,1,1],
+      sign: '♓︎',
+      planet: '♃'
+    },
+    {
+      name: 'Cauda Draconis',
+      numeral: 'XIII',
+      lines: [0,0,0,1],
+      sign: '☋',
+      planet: '♄♂︎'
+    },
+    {
+      name: 'Caput Draconis',
+      numeral: 'XIV',
+      lines: [1,0,0,0],
+      sign: '☊',
+      planet: '♃♀︎'
+    },
+    {
+      name: 'Fortuna Minor',
+      numeral: 'XV',
+      lines: [0,0,1,1],
+      sign: '♌︎',
+      planet: '☉'
+    },
+    {
+      name: 'Via',
+      numeral: 'XVI',
+      lines: [0,0,0,0],
+      sign: '♋︎',
+      planet: '☽'
+    },
+  ]
+})
